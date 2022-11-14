721cc18ba55d255640afb18c7b59ca58e85c8599
caotvph21393 <caotvph21393@fpt.edu.vn>
2022-11-14 20:19:24 +0700

    bắt đầu

---
+++ demogithub/app/src/main/java/caotvph21393/fpoly/demogithub/MainActivity.java
@@ -0,0 +1,14 @@
+package caotvph21393.fpoly.demogithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
